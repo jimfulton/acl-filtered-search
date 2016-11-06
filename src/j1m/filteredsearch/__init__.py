8566175038aebf7c57a6170cfe78115b0c6b84c4
Jim Fulton <jim@zope.com>
2016-11-06 11:31:32 -0500

    unnested path search

--- src/j1m/filteredsearch/__init__.py
+++ src/j1m/filteredsearch/__init__.py
@@ -39,7 +39,61 @@ def filteredsearch(cursor, search, permission, principals, extra=''):
     principals = repr(principals).replace(',)', ')')
     sql = template % dict(
         search=search, permission=permission, principals=principals,
-        extra=extra)
+        extra=extra and ", " + extra)
+    if cursor is None:
+        print(sql)
+    else:
+        cursor.execute(sql)
+        return cursor.fetchall()
+
+path_template = """
+select docid %(extra)s from (
+  select distinct on (docid) s.*, a.allowed
+  from
+    (%(search)s) s
+  join paths p using (docid)
+  join pace a on (
+    p.path like a.pathp and
+    a.permission in ('%(permission)s', '*') and
+    a.who in %(principals)s
+    )
+  order by s.docid, a.pathp desc, a.ord
+) _
+where allowed
+"""
+
+def path_filteredsearch(cursor, search, permission, principals, extra=''):
+    principals = repr(principals).replace(',)', ')')
+    sql = path_template % dict(
+        search=search, permission=permission, principals=principals,
+        extra=extra and ", " + extra)
+    if cursor is None:
+        print(sql)
+    else:
+        cursor.execute(sql)
+        return cursor.fetchall()
+
+upath_template = """
+select docid %(extra)s from (
+  select distinct on (docid) s.*, a.allowed
+  from
+    (%(search)s) s
+  join upaths p using (docid)
+  join ace a on (
+    a.docid = p.ancestor_docid and
+    a.permission in ('%(permission)s', '*') and
+    a.who in %(principals)s
+    )
+  order by s.docid, p.ord, a.ord
+) _
+where allowed
+"""
+
+def upath_filteredsearch(cursor, search, permission, principals, extra=''):
+    principals = repr(principals).replace(',)', ')')
+    sql = upath_template % dict(
+        search=search, permission=permission, principals=principals,
+        extra=extra and ", " + extra)
     if cursor is None:
         print(sql)
     else:
--- src/j1m/filteredsearch/tests.py
+++ src/j1m/filteredsearch/tests.py
@@ -3,6 +3,8 @@ import psycopg2
 import unittest
 
 from j1m.filteredsearch import filteredsearch
+from j1m.filteredsearch import path_filteredsearch
+from j1m.filteredsearch import upath_filteredsearch
 
 class Tests(unittest.TestCase):
 
@@ -12,11 +14,23 @@ class Tests(unittest.TestCase):
         ex = self.ex = cursor.execute
         ex("create temp table docs (docid int)")
         ex("create temp table parents (docid int, parent_docid int)")
-        ex("create temp table ace (docid int, allowed bool, who varchar, permission varchar, ord int)")
+        ex("create temp table paths (docid int, path varchar)")
+        ex("create temp table upaths (docid int, ancestor_docid int, ord int)")
+        ex("""
+        create temp table ace (
+          docid int, allowed bool, who varchar, permission varchar, ord int
+          )
+        """)
 
         def newdoc(docid, parent_docid):
             ex("insert into docs values(%s)", (docid,))
             ex("insert into parents values(%s, %s)", (docid, parent_docid))
+            ex("insert into paths values(%s, %s)", (docid, path(docid)))
+            for ord, ancestor_docid in enumerate(reversed(
+                path(docid)[1:-1].split('/')
+                )):
+                ex("insert into upaths values(%s, %s, %s)",
+                   (docid, ancestor_docid, ord))
             ex("insert into ace values(%s, true, 'decoy', 'decoy', -1)",
                (docid,))
 
@@ -35,14 +49,30 @@ class Tests(unittest.TestCase):
                             m += l*10
                             newdoc(m, l)
 
+        ex("""create temp table pace as
+              select p.path || '%' as pathp,
+                     a.allowed, a.who, a.permission, a.ord
+              from paths p join ace a using (docid)
+              """)
+
+
     def ace(self, *aces):
         for order, (docid, allowed, who, permission) in reversed(
             list(enumerate(aces))):
             self.ex("insert into ace values(%s, %s, %s, %s, %s)",
                     (docid, allowed, who, permission, order))
+            self.ex("insert into pace values(%s, %s, %s, %s, %s)",
+                    (path(docid) + '%', allowed, who, permission, order))
 
     def search(self, permission, *principals):
-        return filteredsearch(self.cursor, "select * from docs", permission, principals)
+        return filteredsearch(self.cursor,
+                              "select * from docs", permission, principals)
+    def path_search(self, permission, *principals):
+        return path_filteredsearch(self.cursor,
+                                   "select * from docs", permission, principals)
+    def upath_search(self, permission, *principals):
+        return upath_filteredsearch(
+            self.cursor, "select * from docs", permission, principals)
 
     def tearDown(self):
         self.conn.close()
@@ -53,40 +83,23 @@ class Tests(unittest.TestCase):
     def test_basic(self):
         self.ace((111, True, "bob", "read"))
         self.ace((112, True, "sally", "edit"))
-        self.assertEqual(self.search("read", "bob"),
-                         [(111,),
-                          (1111,),
-                          (1112,),
-                          (1113,),
-                          (11111,),
-                          (11112,),
-                          (11113,),
-                          (11121,),
-                          (11122,),
-                          (11123,),
-                          (11131,),
-                          (11132,),
-                          (11133,)]
-                         )
+
+        expect = [(111,), (1111,), (1112,), (1113,), (11111,),
+                  (11112,), (11113,), (11121,), (11122,), (11123,),
+                  (11131,), (11132,), (11133,)]
+        self.assertEqual(self.search("read", "bob"), expect)
+        self.assertEqual(self.path_search("read", "bob"), expect)
+        self.assertEqual(self.upath_search("read", "bob"), expect)
 
     def test_true_before_false(self):
         self.ace((111, True, "bob", "read"),
                  (111, False, "bob", "read"))
-        self.assertEqual(self.search("read", "bob"),
-                         [(111,),
-                          (1111,),
-                          (1112,),
-                          (1113,),
-                          (11111,),
-                          (11112,),
-                          (11113,),
-                          (11121,),
-                          (11122,),
-                          (11123,),
-                          (11131,),
-                          (11132,),
-                          (11133,)]
-                         )
+        expect = [(111,), (1111,), (1112,), (1113,), (11111,),
+                  (11112,), (11113,), (11121,), (11122,), (11123,),
+                  (11131,), (11132,), (11133,)]
+        self.assertEqual(self.search("read", "bob"), expect)
+        self.assertEqual(self.path_search("read", "bob"), expect)
+        self.assertEqual(self.upath_search("read", "bob"), expect)
 
     def test_false_before_true(self):
         self.ace(
@@ -96,6 +109,8 @@ class Tests(unittest.TestCase):
             (11,  True, "bob", "read"),
             )
         self.assertEqual(self.search("read", "bob"), [])
+        self.assertEqual(self.path_search("read", "bob"), [])
+        self.assertEqual(self.upath_search("read", "bob"), [])
 
     def test_no_acquire(self):
         self.ace(
@@ -104,34 +119,15 @@ class Tests(unittest.TestCase):
             (111, False, "bob", "read"),
             (111, True, "bob", "read"),
             )
-        self.assertEqual(self.search("read", "bob"),
-                         [(11,),
-                          (112,),
-                          (113,),
-                          (1121,),
-                          (1122,),
-                          (1123,),
-                          (1131,),
-                          (1132,),
-                          (1133,),
-                          (11211,),
-                          (11212,),
-                          (11213,),
-                          (11221,),
-                          (11222,),
-                          (11223,),
-                          (11231,),
-                          (11232,),
-                          (11233,),
-                          (11311,),
-                          (11312,),
-                          (11313,),
-                          (11321,),
-                          (11322,),
-                          (11323,),
-                          (11331,),
-                          (11332,),
-                          (11333,)])
+        expect = [(11,), (112,), (113,), (1121,), (1122,), (1123,),
+                  (1131,), (1132,), (1133,), (11211,), (11212,),
+                  (11213,), (11221,), (11222,), (11223,), (11231,),
+                  (11232,), (11233,), (11311,), (11312,), (11313,),
+                  (11321,), (11322,), (11323,), (11331,), (11332,),
+                  (11333,)]
+        self.assertEqual(self.search("read", "bob"), expect)
+        self.assertEqual(self.path_search("read", "bob"), expect)
+        self.assertEqual(self.upath_search("read", "bob"), expect)
 
     def test_extra_noise(self):
         self.ace(
@@ -142,55 +138,30 @@ class Tests(unittest.TestCase):
             (111, False, "bob", "read"),
             (111, True, "bob", "read"),
             )
-        self.assertEqual(self.search("read", "bob", "all"),
-                         [(11,),
-                          (112,),
-                          (113,),
-                          (1121,),
-                          (1122,),
-                          (1123,),
-                          (1131,),
-                          (1132,),
-                          (1133,),
-                          (11211,),
-                          (11212,),
-                          (11213,),
-                          (11221,),
-                          (11222,),
-                          (11223,),
-                          (11231,),
-                          (11232,),
-                          (11233,),
-                          (11311,),
-                          (11312,),
-                          (11313,),
-                          (11321,),
-                          (11322,),
-                          (11323,),
-                          (11331,),
-                          (11332,),
-                          (11333,)])
-
+        expect = [(11,), (112,), (113,), (1121,), (1122,), (1123,),
+                  (1131,), (1132,), (1133,), (11211,), (11212,),
+                  (11213,), (11221,), (11222,), (11223,), (11231,),
+                  (11232,), (11233,), (11311,), (11312,), (11313,),
+                  (11321,), (11322,), (11323,), (11331,), (11332,),
+                  (11333,)]
+        self.assertEqual(self.search("read", "bob", "all"), expect)
+        self.assertEqual(self.path_search("read", "bob", "all"), expect)
 
     def test_all_permissions(self):
         self.ace((111, True, "bob", "*"))
-        self.assertEqual(self.search("read", "bob"),
-                         [(111,),
-                          (1111,),
-                          (1112,),
-                          (1113,),
-                          (11111,),
-                          (11112,),
-                          (11113,),
-                          (11121,),
-                          (11122,),
-                          (11123,),
-                          (11131,),
-                          (11132,),
-                          (11133,)]
-                         )
+        expect = [(111,), (1111,), (1112,), (1113,), (11111,),
+                  (11112,), (11113,), (11121,), (11122,),
+                  (11123,), (11131,), (11132,), (11133,)]
+        self.assertEqual(self.search("read", "bob"), expect)
+        self.assertEqual(self.path_search("read", "bob"), expect)
+        self.assertEqual(self.upath_search("read", "bob"), expect)
 
 
+def path(docid):
+    if docid:
+        return path(docid // 10) + (str(docid) + '/')
+    else:
+        return '/'
 
 def test_suite():
     return unittest.makeSuite(Tests)
